210d06ba8fb00825da2a8669a72eaf485a54ce62
Jaden Ficklin <86342418+JadenFicklin@users.noreply.github.com>
2021-08-10 15:04:31 -0600

    task 1d

--- index.js
+++ index.js
@@ -62,9 +62,10 @@ Do the following:
    3. Multiply a and b and return the answer
 */
 
-function multiply(/*add your code here*/) {
-  /*add your code here*/
+function multiply(a, b) {
+  return a * b;
 }
+console.log(multiply(10, 2));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
